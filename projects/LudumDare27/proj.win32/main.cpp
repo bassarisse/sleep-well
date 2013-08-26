839dbca9585c9b7eb2a43b6e573053f4981c7d55
Bruno Assarisse <bruno@assarisse.com.br>
2013-08-26 15:29:55 -0300

    Icon and game name.

--- projects/LudumDare27/proj.win32/main.cpp
+++ projects/LudumDare27/proj.win32/main.cpp
@@ -2,6 +2,8 @@
 #include "AppDelegate.h"
 #include "CCEGLView.h"
 
+#include "resource.h"
+
 USING_NS_CC;
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
@@ -15,7 +17,15 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     // create the application instance
     AppDelegate app;
     EGLView* eglView = EGLView::getInstance();
-    eglView->setViewName("LudumDare27");
-    eglView->setFrameSize(480, 320);
+    eglView->setViewName("Sleep Well");
+    eglView->setFrameSize(960, 640);
+    
+	HICON hIcon = LoadIcon( GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1) );
+	if( hIcon )
+	{
+        SendMessage( eglView->getHWnd(), WM_SETICON, ICON_BIG, (LPARAM)hIcon );
+        DestroyIcon( hIcon );
+	}
+    
     return Application::getInstance()->run();
 }
